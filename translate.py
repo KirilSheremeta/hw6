32455aebebd1368083830a503365f2e59273140b
v.dunkan <v.dunkan@gmail.com>
2022-12-17 23:12:21 +0200

    by Skiff

--- translate.py
+++ translate.py
@@ -1,10 +1,43 @@
 cyrillic_symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-translation = ("a", "b", "v", "g", "d", "e", "e",
-               "j", "z", "i", "j", "k", "l", "m",
-               "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch",
-               "", "y", "", "e", "ju", "ja", "je",
-               "i", "ji", "g")
+translation = (
+    "a",
+    "b",
+    "v",
+    "g",
+    "d",
+    "e",
+    "e",
+    "j",
+    "z",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "f",
+    "h",
+    "ts",
+    "ch",
+    "sh",
+    "sch",
+    "",
+    "y",
+    "",
+    "e",
+    "ju",
+    "ja",
+    "je",
+    "i",
+    "ji",
+    "g",
+)
 translations = {}
 
 for i, j in zip(cyrillic_symbols, translation):
@@ -12,19 +45,28 @@ for i, j in zip(cyrillic_symbols, translation):
     translations[ord(i.upper())] = j.upper()
 
 
-def normalize_names(name):
-    """ This function normalizing from Cyrillic to Latin"""
+def normalize_names(name: str):
+    """This function normalizing from Cyrillic to Latin"""
 
-    result = ""
-    try:
-        for symbol in name.split(".")[0]:
-            if symbol.lower() not in cyrillic_symbols and symbol.lower() not in translation and symbol not in "1234567890cxwXCW":
-                result += "_"
-            elif symbol.lower() not in cyrillic_symbols:
-                result += symbol
-            else:
-                result += translations[ord(symbol)]
-    except:
-        return name
-    result += "." + name.split(".")[1]
-    return result
+    # result = ""
+    # try:
+    #     for symbol in name.split(".")[0]:
+    #         if (
+    #             symbol.lower() not in cyrillic_symbols
+    #             and symbol.lower() not in translation
+    #             and symbol not in "1234567890cxwXCW"
+    #         ):
+    #             result += "_"
+    #         elif symbol.lower() not in cyrillic_symbols:
+    #             result += symbol
+    #         else:
+    #             result += translations[ord(symbol)]
+    # except:
+    #     return name
+    # result += "." + name.split(".")[1]
+
+    return name.translate(translations)
+
+
+if __name__ == "__main__":
+    print(normalize_names("русні пизда"))
