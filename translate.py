fbd015ef5a07e2312de65c7411076e0bed12bded
kirilsheremeta <120675223+kirilsheremeta@users.noreply.github.com>
2022-12-24 19:49:04 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,10 +1,6 @@
 cyrillic_symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-translation = ("a", "b", "v", "g", "d", "e", "e",
-               "j", "z", "i", "j", "k", "l", "m",
-               "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch",
-               "", "y", "", "e", "ju", "ja", "je",
-               "i", "ji", "g")
+translation = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r",
+               "s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "ju", "ja", "je", "i", "ji", "g",)
 translations = {}
 
 for i, j in zip(cyrillic_symbols, translation):
@@ -12,13 +8,17 @@ for i, j in zip(cyrillic_symbols, translation):
     translations[ord(i.upper())] = j.upper()
 
 
-def normalize_names(name):
-    """ This function normalizing from Cyrillic to Latin"""
+def normalize_names(name: str):
+    """This function normalizing from Cyrillic to Latin"""
 
     result = ""
     try:
         for symbol in name.split(".")[0]:
-            if symbol.lower() not in cyrillic_symbols and symbol.lower() not in translation and symbol not in "1234567890cxwXCW":
+            if (
+                symbol.lower() not in cyrillic_symbols
+                and symbol.lower() not in translation
+                and symbol not in "1234567890cxwXCW"
+            ):
                 result += "_"
             elif symbol.lower() not in cyrillic_symbols:
                 result += symbol
@@ -27,4 +27,8 @@ def normalize_names(name):
     except:
         return name
     result += "." + name.split(".")[1]
-    return result
+    return name.translate(translations)
+
+
+if __name__ == "__main__":
+    print(normalize_names("русні пизда"))
